de5a91c7718e63e22e07da20d6dfcd031b5ef971
Jake Holbein <jholbein@augusta.edu>
2022-12-01 11:11:39 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Dijkstras_Algorithm
+{
+    internal class Program
+    {
+        //Set the starting node or source node's distance to 0
+
+        //while (q is not empty)
+        //{
+            //u = node in Q with min distance 
+            //remove u from Q
+            //foreach outgoing edge of u
+            //{
+                //calculate a new distance going thru u
+                //if(new distance < old distance in table
+                //{
+                    //update table's distance and parent
+                    //update distance in Q
+                //}
+            //}
+        //}
+        
+        //Network flow problems
+            //a flow network is a directed graph with two special verticies
+                //s- the source
+                //t- the sink
+            //Each edge represents the capacity of the link
+            //incoming flow = outgoing flow
+            
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
